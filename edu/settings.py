fd9df5622e2c946f5f627424e63b408b6ce3bcd0
linguoyang <linguoyang2008@126.com>
2016-10-30 02:00:03 +0800

    广告和文章功能

--- edu/settings.py
+++ edu/settings.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding:utf-8
+
 """
 Django settings for edu project.
 
@@ -39,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'app.common',
 ]
 
 MIDDLEWARE = [
@@ -107,13 +110,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# 扩展user类，防止同步数据库时出现的“同名”错误
+AUTH_USER_MODEL = 'common.UserProfile' #common是app名字，UserProfile是类名
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -121,9 +127,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'ad'),
+)
+
+# 文件上传的路径
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'uploads')
